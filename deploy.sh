74a4bf732c0d92c9d3df11c0504714541683984a
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2017-02-24 16:40:34 -0500

    Better deployment script

--- deploy.sh
+++ deploy.sh
@@ -2,10 +2,6 @@
 
 HOST="kekstarter.org"
 
-# Deletes build/test directories
-rm -r client/target
-rm -r client/src/test
-
 # Generates the guide
 pandoc -V geometry:margin=1in client/README.md -o client/README.pdf
 
@@ -13,10 +9,7 @@ pandoc -V geometry:margin=1in client/README.md -o client/README.pdf
 sed -i "s/localhost/${HOST}/" client/pom.xml
 
 # Compresses the client
-zip -r cybercycles.zip client -x README.md
+zip -r cybercycles.zip client -x client/README.md client/target client/src/test
 
 # Edits back the host
 sed -i "s/${HOST}/localhost/" client/pom.xml
-
-# Deletes the generated guide
-rm client/README.pdf
